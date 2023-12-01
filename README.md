2b4f1b5f9d4706f385d20583af6415beb7541e9b
unknown <ebeck703@gmail.com>
2023-11-30 20:33:55 -0500

    Final commit. Added all questions and answer choice buttons to display properly. Finished CSS to format texts, change font sizes, and change colors to the buttons. Also added the submit score feature and timer to work.

---
+++ README.md
@@ -0,0 +1,23 @@
+The USER STORY with this project was to create a working Coding quiz that stores high scores so that progress can be gauged compared to peers.
+
+This is done when the acceptance criteria is met which includes - 
+A start button to begin the quiz. 
+This is followed by a timer that starts counting down and five coding questions. 
+When a question is answered, the user is presented with another question, until they reach the end of the quiz. 
+If a question is entered incorrectly, ten seconds is subtracted from the quiz. 
+When all questions are answered or the timer reaches 0, the quiz ends and the game is over.
+When the game ends, the user can enter their initials and score to be saved on the leader-board. 
+
+For my final product, I was unable to make the final score display properly. It displays as "Quiz Over! Your score is 0s - EB." 
+
+The score does change, but it ranges from -10 to -60s, then after submitting it displays a 0. I was unable to figure out this error. 
+
+I also decided to spice it up with the questions, some involving back-end languages, rather than being a strictly JavaScript based quiz. 
+
+I also could not figure out how to make scores save or create a leader-board sadly. 
+
+The final thing that I could not fix was the timer element. When a wrong answer is clicked, nothing happens. No time is lost, and you do not move to the next question. 
+
+I was able to meet the rest of the acceptance criteria, everything else displayed properly. All choice boxes were even, the text was formatted and colored coherently the way I wanted it to be. 
+
+Here is the link to the live server
--- script.js
+++ script.js
@@ -2,41 +2,187 @@
 const questions = [
     {
         question: "What does HTML stand for?",
-        choices: ["Meow", "Woof", "Hello mate", "Nothing"],
+        choices: ["Hyper Text Markup Language", "High Tech Machine Learning", "Hyper Transferable Module Language", "Home Tool Management Language"],
         correctAnswer: 0
     },
-{
-    question: "what does SQL stand for?",
-    choices: ["structured Query Language", "Sequential Query Language", "Simple Question Language", "Syntax Query Language"],
-    correctAnswer: 0
-},
-{
-    question: "What does MVC stand for in the context of web development?",
-    choices: ["Model View Controller", "Multiple View Control", "Model Visual Component", "Multifunctional View Controller"],
-    correctAnswer: 0
-  }
+    {
+        question: "what does SQL stand for?",
+        choices: ["Structured Query Language", "Sequential Query Language", "Simple Question Language", "Syntax Query Language"],
+        correctAnswer: 0
+    },
+    {
+        question: "What does MVC stand for in the context of web development?",
+        choices: ["Model View Controller", "Multiple View Control", "Model Visual Component", "Multifunctional View Controller"],
+        correctAnswer: 0
+    },
+    {
+        question: "Which of the following is a programming language?",
+        choices: ["HTML", "CSS", "JavaScript", "XML"],
+        correctAnswer: 2
+    },
+    {
+        question: "Which of the following terms are JavaScript syntax?",
+        choices: [
+            "If-else",
+            "Who-else",
+            "When-else",
+            "Where-else"
+        ],
+        correctAnswer: [0]
+    }
 ];
 
-let currentQuestionsIndex = 0;
+let remainingTime = 0;
+let currentQuestionIndex = 0;
 let score = 0;
-let timer;
-const timeLimitInSeconds = 60;
+let timer = null;
+let timeLimitInSeconds = 60; // This let will reset to 60 seconds for each quiz.
 
 // Connecting JS to grab HTML elements
 const startBtn = document.getElementById('start-btn');
-const quizContainer =document.getElementById('quiz-container');
+const quizContainer = document.getElementById('quiz-container');
 const questionContainer = document.getElementById('question-container');
 const resultContainer = document.getElementById('result-container');
+const timerContainer = document.getElementById('timer-container');
+const saveScoreContainer = document.getElementById('save-score-container');
+const initialsInput = document.getElementById('initials-input');
+const submitBtn = document.getElementById('submit-btn');
+const backBtn = document.getElementById('back-btn');
 
-// Click event for the start button
+// Click event for the start button.
 startBtn.addEventListener('click', startQuiz);
 
-// Function so the quiz can be started
+// Function so the quiz can begin.
 function startQuiz() {
-    startBtn.style.display ='none';
+    startBtn.style.display = 'none';
     quizContainer.style.display = 'block';
 
-    // Display for question and timer
+    // This is a display for question and timer.
     showQuestion();
     startTimer();
 }
+
+// This function will show the questions, currentQuestionIndex ensures the questions go in the order they are entered in.
+function showQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    questionContainer.textContent = currentQuestion.question;
+
+    clearChoiceButtons();
+
+    // New buttons for the choices.
+    currentQuestion.choices.forEach((choice, index) => {
+        const choiceBtn = document.createElement('button');
+        choiceBtn.textContent = choice;
+        choiceBtn.className = 'choice-btn';
+        choiceBtn.addEventListener('click', () => checkAnswer(index));
+        document.getElementById('quiz-container').appendChild(choiceBtn);
+    });
+}
+
+// This function will clear existing choice buttons.
+function clearChoiceButtons() {
+    const quizContainer = document.getElementById('quiz-container');
+    const existingButtons = document.querySelectorAll('.choice-btn');
+    
+    existingButtons.forEach(button => {
+        quizContainer.removeChild(button);
+    });
+}
+
+// This function will check the answer to see if they're correct.
+function checkAnswer(choiceIndex) {
+    console.log(`Checking answer for index ${choiceIndex}`);
+    console.log(`Before time deduction: timeLimitInSeconds = ${timeLimitInSeconds}`);
+    
+    // If statement checks to see if there is a valid currentQuestion.
+    if (currentQuestionIndex < questions.length) {
+        const currentQuestion = questions[currentQuestionIndex];
+        
+        if (choiceIndex === currentQuestion.correctAnswer) {
+            score++;
+        } else {
+            timeLimitInSeconds -= 10;
+            if (timeLimitInSeconds < 0) {
+                timeLimitInSeconds = 0;
+            }
+        }
+
+        console.log(`Current question: ${currentQuestion.question}`);
+        console.log(`Score: ${score}`);
+        console.log(`Remaining time: ${timeLimitInSeconds}`);
+        
+        // If answer is correct, test moves to next question.
+        currentQuestionIndex++;
+
+        console.log(`After moving to the next question: currentQuestionIndex = ${currentQuestionIndex}`);
+
+        if (currentQuestionIndex < questions.length) {
+            showQuestion();
+        } else {
+            endQuiz();
+        }
+    } else {
+        // Handle the scenario when all questions are answered
+        console.log("All questions answered");
+        endQuiz();
+    }
+}
+  
+
+// This function starts the timer countdown.
+function startTimer() {
+    let timeRemaining = timeLimitInSeconds;
+    timerContainer.textContent = `Time: ${timeRemaining}s`;
+
+    // Interval function updates the timer every second (1000 milliseconds).
+    timer = setInterval(() => {
+        if (timeRemaining <= 0) {
+            endQuiz();
+        } else {
+            timeRemaining--;
+            timerContainer.textContent = `Time: ${timeRemaining}s`;
+        }
+    }, 1000);
+}
+
+// This end function will end the quiz.
+function endQuiz() {
+    clearInterval(timer);
+
+    const initialTime = 60;
+    const elapsedTime = timeLimitInSeconds;
+    const remainingTime = Math.max(0, initialTime - elapsedTime);
+
+    quizContainer.style.display = 'none';
+    resultContainer.textContent = `Quiz Over! Your score is ${remainingTime}s`;
+    
+    saveScoreContainer.style.display = 'block';
+}
+
+// This is a click event for the submit button.
+submitBtn.addEventListener('click', saveScore);
+
+// Click event for the back button.
+backBtn.addEventListener('click', () => {
+    saveScoreContainer.style.display = 'none';
+    startBtn.style.display = 'block';
+    currentQuestionIndex = 0;
+    score = 0;
+    timeLimitInSeconds = 60;
+});
+
+// This function will save the score.
+function saveScore() {
+    const initials = initialsInput.value.toUpperCase(); // Convert to uppercase for consistency
+    console.log(`Initials: ${initials}, Score: ${score}`);
+
+    resultContainer.textContent = `Quiz Over! Your score is ${remainingTime}s - ${initials}`;
+   
+    saveScoreContainer.style.display = 'none';
+    startBtn.style.display = 'block';
+    currentQuestionIndex = 0;
+    score = 0;
+    timeLimitInSeconds = 60;
+}
+
+  
